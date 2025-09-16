6746bd9c90b4d6d7b1196e0d0e464d447c3ddc79
LuisRobles <lrobles.vnty@gmail.com>
2025-09-16 17:26:39 -0600

    Update Vite configuration base path to root for consistent deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //base:process.env.VITE_BASE_PATH || '/react-curso',
-  base:'./',
+  base:'/',
   build: {
     chunkSizeWarningLimit: 1600,
   },
